f09592e366faf30476d101de3051a7a66063c057
Bimo Ichmu <25116900+bimoich@users.noreply.github.com>
2023-01-23 16:12:31 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Homepage</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, itaque dignissimos minus illum nostrum
+            velit sequi odio molestiae culpa fuga?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, itaque dignissimos minus illum nostrum
+            velit sequi odio molestiae culpa fuga?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Product Details | Id: {{ id }}</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+definePageMeta({
+    layout: 'products'
+})
+</script>
+
+<style scoped>
+
+</style>
