b8014233f58028ed38d414a34d0db77a02f8ca3b
Bimo Ichmu <25116900+bimoich@users.noreply.github.com>
2023-02-18 22:08:33 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,23 +3,24 @@
         <header class="shadow-sm bg-white">
             <nav class="nav-bar">
                 <a href="/" class="home-btn">Website X</a>
-                <!-- <NuxtLink to="/" class="home-btn">Website X</NuxtLink> -->
                 <div class="flex-row">
-                    <ul class="flex gap-8">
+                    <ul class="flex gap-12">
                         <NuxtLink class="nav-link" to="/">About</NuxtLink>
                         <NuxtLink class="nav-link" to="/about">Medical Checkup</NuxtLink>
                         <NuxtLink class="nav-link" to="/products">Promo & News</NuxtLink>
                         <div>
                             <label>Follow us: </label>
-                            <a v-for="link in socialLinks" :key="link.name" :href="link.url" target="blank" class="mx-2">
+                            <a v-for="link in socialLinks" :key="link.name" :href="link.url" target="blank" class="mx-4">
                                 <font-awesome-icon :icon="['fa-brands', link.icon]" class="social-icons"/>
                             </a>
                         </div>
                     </ul>
-                    <ul class="flex gap-8">
+                    <ul class="flex gap-12">
                         <NuxtLink class="nav-link" to="/">Home</NuxtLink>
-                        <NuxtLink class="nav-link" to="/about">About</NuxtLink>
-                        <NuxtLink class="nav-link" to="/products">Products</NuxtLink>
+                        <NuxtLink class="nav-link" to="/about">Find a doctor</NuxtLink>
+                        <NuxtLink class="nav-link" to="/products">Book appointment</NuxtLink>
+                        <NuxtLink class="nav-link" to="/products">Hospitals</NuxtLink>
+                        <NuxtLink class="nav-link" to="/products">Center of excellence</NuxtLink>
                     </ul>
                 </div>
             </nav>
